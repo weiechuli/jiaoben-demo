875d52e25d66242fb61852ca02e97b2b3266ef96
wei hang <2764590350@qq.com>
2022-08-18 11:05:44 +0800

    create temp.js

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        hello world
+// @namespace    http://mfds.cn/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.log('hello world');
+})();
